de6d0c0dbd1dd4db54b4733ce4dddfc069fb8930
KarenP943 <160674417+KarenP943@users.noreply.github.com>
2024-03-02 21:14:23 -0500

    Add files via upload

---
+++ ejercicio_java/Ejercicio4.js
@@ -0,0 +1,13 @@
+// Entrada
+
+console.log ("4.Convertir euros a dolares");
+console.log("Ingrese el valor de euros a convertir: ");
+
+let valoreuro= 100;
+let valordolar= 1.07747;
+
+//Proceso
+let resultado =(valoreuro*valordolar);
+
+//salida
+console.log(resultado);
