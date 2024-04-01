0137367aaaed006ef0a9ab62dd978df6966a93b3
KarenP943 <160674417+KarenP943@users.noreply.github.com>
2024-04-01 04:07:18 -0500

    Delete Ciclos directory

--- Ciclos/Ejercicio4.js
+++
@@ -1,4 +0,0 @@
-for (let i = 1; i <= 30; i++) {
-    let cuadrado = i * i;
-    console.log(`El cuadrado de ${i} es: ${cuadrado}`);
-  }
--- Ciclos/Ejercicio5.js
+++
@@ -1,6 +0,0 @@
-let sumaCuadrados = 0;
-for (let i = 1; i <= 100; i++) {
-  let cuadrado = i * i;
-  sumaCuadrados += cuadrado;
-}
-console.log(`La suma de los cuadrados de los cien primeros números naturales es: ${sumaCuadrados}`);
